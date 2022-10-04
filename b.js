59beacbbd4fad3464722b849b7cd4d857674f0e1
Spew45 <103700395+Spew45@users.noreply.github.com>
2022-10-03 17:30:59 -0700

    fix

---
+++ b.js
@@ -0,0 +1,9 @@
+document.write('\
+\
+    <p align="center"><iframe src="https://spew45.github.io/webretro/index.html?core=autodetect&rom=https://spew45.github.io/new-website/webretro/roms/super_mario_bros.nes" style="border: 1px solid #f6ff00; height: 425px; width: 425px;" id="iframe">\
+\
+     <p>Notice however, that you have to escape LF's with a '\', just like\
+        demonstrated in this code listing.\
+    </p>\
+\
+');
